c2ccb89c5d6b32fff531c4aa24d93e1cd3f4d961
Fernando P. Najera Cano <f.najera@rac.de>
2014-07-25 11:07:12 +0200

    PushIgnoreNamespace for subscripts (see <https://groups.google.com/forum/?hl=en#!msg/cs-script/tpnXi3TZC8M/YE-kig4WhKYJ>)

--- fileparser.cs
+++ fileparser.cs
@@ -727,6 +727,9 @@ namespace CSScriptLibrary
                     foreach (string file in importedFile.Precompilers)
                         PushPrecompiler(file);
 
+                    foreach (string file in importedFile.IgnoreNamespaces)
+                        PushIgnoreNamespace(file);
+
                     foreach (ScriptInfo scriptFile in importedFile.ReferencedScripts)
                         ProcessFile(scriptFile);
 
